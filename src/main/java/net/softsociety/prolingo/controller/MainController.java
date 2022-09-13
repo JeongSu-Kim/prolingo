b83649d3738d19523fe2ce28710e82d534c5786c
tmdqja92 <110378945+tmdqja92@users.noreply.github.com>
2022-09-13 15:24:27 +0900

    학습코스 페이지

--- src/main/java/net/softsociety/prolingo/controller/MainController.java
+++ src/main/java/net/softsociety/prolingo/controller/MainController.java
@@ -2,11 +2,14 @@ package net.softsociety.prolingo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
+@RequestMapping("test")
 public class MainController {
 	
 	@GetMapping({"","/"})
